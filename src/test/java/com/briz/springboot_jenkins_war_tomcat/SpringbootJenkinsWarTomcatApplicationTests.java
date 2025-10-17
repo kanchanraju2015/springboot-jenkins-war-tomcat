06ee83802bf2889535e80e24f970162a5b60e89c
kanchan raju <kanchanraju2015@gmail.com>
2025-10-17 14:12:10 +0530

    war file created

---
+++ src/test/java/com/briz/springboot_jenkins_war_tomcat/SpringbootJenkinsWarTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_jenkins_war_tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsWarTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
